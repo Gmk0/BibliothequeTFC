ab6b57552ec0063f28cef05f967c4b0aab8b3198
gmk0 <mkgeorges63@gmail.com>
2022-12-08 22:04:11 +0100

    add very well login container

--- resources/views/components/navbarUser.blade.php
+++ resources/views/components/navbarUser.blade.php
@@ -1,8 +1,10 @@
 <div class="">
+    
     <div class="wrapper sticky-top">
-        <nav class="">
+        <nav>
             <input type="checkbox" id="show-search">
             <input type="checkbox" id="show-menu">
+            <input type="checkbox" id="show-login">
             <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
             <div class="content">
                 <div class="logo"><a href="#">Bibliotheque UCC </a></div>
@@ -42,14 +44,20 @@
                         </ul>
                     </li>
                     @else
-                    <li class="mr-2 ml-2"><a href="{{route('login')}}" class="sign">se connecter</a></li>
-                    <li class="mr-2 ml-2"><a href="{{route('register')}}" class="login">S'inscrire</a></li>
+                   
                     @endauth
 
                 </ul>
 
             </div>
-            <label for="show-search" class="search-icon"><i class="fas fa-user"></i></label>
+            <div class="login">
+                <ul class="linkLogin">
+                    <li><a class="hover login" data-toggle="modal" data-target="#modal-lg" href="">login</a></li>
+                    <li><a class="hover sign" href="">register</a></li>
+                   </ul>
+            </div>
+         
+            <label for="show-login" class="user-icon"><i class="fas fa-user"></i></label>
             <label for="show-search" class="search-icon"><i class="fas fa-search"></i></label>
             <form action="#" class="search-box">
                 <input type="text" placeholder="Type Something to Search..." required>
@@ -57,4 +65,49 @@
             </form>
         </nav>
     </div>
+
+    <div class="modal fade" id="modal-lg" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+        aria-hidden="true">
+        <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">domaine</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-12 col-form-label ">{{ __('file') }}</label>
+
+                            <div class="form-goup">
+                                <input id="name" type="file" class="form-control @error('file') is-invalid @enderror"
+                                    autocomplete="name" autofocus ="file">
+
+                                @error('file')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal"
+                                >Close</button>
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+
+
+                    </form>
+
+                </div>
+
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    
 </div>
