9974d1623b499cc3ea39ca33484d8b46cef379c2
gmk0 <mkgeorges63@gmail.com>
2022-12-22 13:00:45 +0100

    most modification

---
+++ resources/views/vendor/jetstream/components/form-section.blade.php
@@ -0,0 +1,27 @@
+@props(['submit'])
+
+<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
+    <x-jet-section-title>
+        <x-slot name="title">{{ $title }}</x-slot>
+        <x-slot name="description">{{ $description }}</x-slot>
+    </x-jet-section-title>
+
+    <div class="mt-5 md:mt-0 md:col-span-2">
+        <form wire:submit.prevent="{{ $submit }}">
+            <div class="px-4 py-5 bg-white sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
+                <div class="grid grid-cols-6 gap-6">
+                    {{ $form }}
+                </div>
+            </div>
+
+            @if (isset($actions))
+                <div class="row justify-content-start w-25 my-2 p-2">
+                    <div class="col-md-6 ">
+                        {{ $actions }}
+                    </div>
+                    
+                </div>
+            @endif
+        </form>
+    </div>
+</div>
